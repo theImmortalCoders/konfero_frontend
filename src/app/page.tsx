2bdf792170e81ba7d7aadb7a78ac07d0582c65c3
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2024-03-20 00:01:15 +0100

    feat: #2 RWD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,35 +7,33 @@ export default function Home() {
   return (
     <main className="flex min-h-max min-w-screen bg-black2darkblue-gradient justify-center items-center mt-navbar">
       <Box>
-        <div className="grid grid-cols-2 w-full h-full">
-          <div className="pr-8">
-            <h1 className="text-4xl font-bold mb-6">Witamy w Konferno</h1>
-            <p className="text-xl mb-8">
+        <div className="grid grid-cols-1 xl:grid-cols-2 w-full h-full">
+          <div className="pr-0 md:pr-8">
+            <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-6">
+              Witamy w Konferno
+            </h1>
+            <p className="text-md sm:text-lg md:text-xl mb-4 sm:mb-6 md:mb-8">
               Najlepszej stronie do organizacji konferencji
             </p>
-            <p className="text-xl mb-8">
+            <p className="text-md sm:text-lg md:text-xl mb-4 sm:mb-6 md:mb-8">
               Zapewniamy niezawodne planowanie konferencji, rejestrację
               uczestników , zarządzanie harmonogramem sesji i prezentacji oraz
               publikację materiałów konferencyjnych
             </p>
-            <p className="text-xl mb-8">
+            <p className="text-md sm:text-lg md:text-xl mb-4 sm:mb-6 md:mb-8">
               Rozsiądz się wygodnie i zostaw wszystko nam...
             </p>
             <div className="flex items-center justify-center w-full">
-              <button className="bg-close2White text-blue text-lg font-medium py-3 px-6 rounded-2xl flex items-center justify-center">
+              <button className="bg-close2White text-blue text-sm sm:text-md lg:text-lg font-medium py-2 px-4 md:py-3 md:px-6 rounded-2xl flex items-center justify-center">
                 <a href="/login">Dołącz do nas</a>
-                <div className="ml-6 w-9 h-9 rounded-full bg-blue flex items-center justify-center">
+                <div className="ml-3 md:ml-6 w-6 h-6 md:w-9 md:h-9 rounded-full bg-blue flex items-center justify-center">
                   <FaArrowRight className="text-close2White" />
                 </div>
               </button>
             </div>
           </div>
-          <div className="w-full h-full flex items-center">
-            <Image
-              src={Laptop}
-              alt="LaptopImage"
-              className="h-[384px] w-[640px] rounded-3xl"
-            />
+          <div className="w-full h-full hidden xl:flex items-center">
+            <Image src={Laptop} alt="LaptopImage" className=" rounded-3xl" />
           </div>
         </div>
       </Box>
--- src/components/common/Box/Box.tsx
+++ src/components/common/Box/Box.tsx
@@ -1,6 +1,6 @@
 export default function Box({ children }: { children: React.ReactNode }) {
   return (
-    <main className="items-start h-auto w-[70%] bg-darkblue2blue-gradient z-0 rounded-3xl px-8 py-8 text-close2White">
+    <main className="items-start h-auto w-[90%] lg:w-[70%]  bg-darkblue2blue-gradient z-0 rounded-3xl px-8 py-8 text-close2White">
       {children}
     </main>
   );
