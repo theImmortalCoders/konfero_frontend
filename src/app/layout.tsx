68ae912b930ccbc2d30a83ac7e61dfc3e8e99d8e
Bartłomiej Krówka <157894166+bkrowka@users.noreply.github.com>
2024-03-20 14:09:36 +0100

    feat: #6 Created footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/common/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pl">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Footer/>
+      </body>
     </html>
   );
 }
---
+++ src/components/common/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer () {
+    return (
+        <footer>
+            <div className="flex flex-row items-center justify-center h-52 space-x-8">
+                <span>1</span>
+                <span>2</span>
+                <span>3</span>
+            </div>
+        </footer>
+    )
+}
