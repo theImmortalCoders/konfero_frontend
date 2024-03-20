54f26f7dc15674b3911ae3c045177db81700eb49
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2024-03-20 13:32:57 +0100

    feat: #5 some changes

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -1,3 +1,30 @@
+import Link from "next/link";
+import { FaBars } from "react-icons/fa6";
+
 export default function Navbar() {
-  return <nav className="w-full h-navbar bg-close2White"></nav>;
+  return (
+    <nav className="w-full h-navbar bg-close2White flex justify-between items-center px-20 font-thin text-black">
+      <FaBars className="flex md:hidden" />
+      <button>
+        <Link href="/">Home</Link>
+      </button>
+      <div className="flex justify-around items-center md:h-full w-[40%]">
+        <button>
+          <Link href="/about">O nas</Link>
+        </button>
+        <button>
+          <Link href="/findevent">Znajdz wydarzenie</Link>
+        </button>
+        <button>
+          <Link href="/findevent">Znajdz wydarzenie</Link>
+        </button>
+        <button>
+          <Link href="/findevent">Znajdz wydarzenie</Link>
+        </button>
+      </div>
+      <button>
+        <Link href="/login">Login</Link>
+      </button>
+    </nav>
+  );
 }
