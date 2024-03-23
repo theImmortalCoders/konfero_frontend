917d763e885657c9a7d0fac7709be27f1dec4995
Bartłomiej Krówka <157894166+bkrowka@users.noreply.github.com>
2024-03-24 00:11:46 +0100

    fix: #6 Footer RWD

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -4,28 +4,28 @@ import { FaXTwitter } from "react-icons/fa6";
 export default function Footer () {
     return (
         <footer>
-            <div className="flex justify-center px-6 py-5">
-                <span className="w-full">
+            <div className="flex flex-col lg:flex-row px-6 py-5">
+                <span className="flex w-full justify-center lg:justify-start order-2 lg:order-1 ">
                     <img src="/logo/blue/logo_img_blue.png" alt="logo" className="w-12 h-12"/>
                 </span>
-                <span className="flex w-full justify-end items-center space-x-6 text-darkblue">
-                    <FaFacebook className="w-8 h-8"/>
-                    <FaInstagram className="w-8 h-8"/>
-                    <FaXTwitter className="w-8 h-8"/>
-                    <FaYoutube className="w-8 h-8"/>
-                    <FaTwitch className="w-8 h-8"/>
+                <span className="flex w-full justify-center lg:justify-end items-center space-x-6 mb-6 lg:mb-0 text-darkblue order-1 lg:order-2">
+                    <FaFacebook className="w-8 h-8 cursor-pointer"/>
+                    <FaInstagram className="w-8 h-8 cursor-pointer"/>
+                    <FaXTwitter className="w-8 h-8 cursor-pointer"/>
+                    <FaYoutube className="w-8 h-8 cursor-pointer"/>
+                    <FaTwitch className="w-8 h-8 cursor-pointer"/>
                 </span>
             </div>
-            <div className="flex justify-center px-6 py-5 text-blue border-t border-blue">
-                <span className="flex w-full">
+            <div className="flex flex-col lg:flex-row px-6 py-5 text-blue border-t border-blue">
+                <span className="flex w-full justify-center lg:justify-start py-4 lg:py-0 order-2 lg:order-1">
                     <p className="font-sans text-xs">KONFERNO © 2024 Wszelkie prawa zastrzeżone</p>
                 </span>
-                <span className="flex w-full justify-end items-center space-x-6">
-                    <p className="font-sans text-xs">ZOBACZ SPONSORÓW</p>
-                    <p className="font-sans text-xs">Ostrzeżenie prawne</p>
-                    <p className="font-sans text-xs">Polityka prywatności</p>
-                    <p className="font-sans text-xs">Polityka plików cookies</p>
-                    <p className="font-sans text-xs">Kanał informacyjny</p>
+                <span className="flex flex-wrap lg:flex-nowrap w-full justify-center lg:justify-end items-center space-x-6 order-1 lg:order-2">
+                    <p className="font-sans text-xs cursor-pointer">Sponsorzy</p>
+                    <p className="font-sans text-xs cursor-pointer">Ostrzeżenie prawne</p>
+                    <p className="font-sans text-xs cursor-pointer">Polityka prywatności</p>
+                    <p className="font-sans text-xs cursor-pointer">Polityka plików cookies</p>
+                    <p className="font-sans text-xs cursor-pointer">Kanał informacyjny</p>
                 </span>
             </div>
         </footer>
