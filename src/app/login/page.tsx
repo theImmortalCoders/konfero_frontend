b1a211306c11f51a47359894bfa8d5aa60f5caaa
Bartłomiej Krówka <157894166+bkrowka@users.noreply.github.com>
2024-03-27 17:15:20 +0100

    fix: #11 Button small update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,9 +13,11 @@ function LoginBoard () {
         <div className="flex flex-col w-11/12 justify-center items-center text-center">
             <p className="text-4xl mb-8">Witaj!</p>
             <p className="text-lg mb-8">Zaloguj się, aby uzyskać dostęp:</p>
-            <button className="flex justify-center items-center w-full md:w-96 h-16 text-white text-lg bg-blue rounded-2xl px-8">
-                <FaGoogle className="text-base mr-2"/> <p className="text-sm xs:text-lg">Kontynuuj z Google</p>
-            </button>
+            <a href="localhost:8080/api/oauth2/authorize/google">
+                <button className="flex justify-center items-center w-full md:w-96 h-16 text-white text-lg bg-blue rounded-2xl px-8">
+                    <FaGoogle className="text-base mr-2"/> <p className="text-sm xs:text-lg">Kontynuuj z Google</p>
+                </button>
+            </a>
             <p className="text-xs font-sans w-64 mt-4">Przetworzymy Twój adres e-mail, aby sprawdzić, czy jesteś już zarejestrowany.</p>
     </div>
     )
