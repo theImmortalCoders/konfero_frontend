f762abb1a04b2ba16650b84ce41094b551e5bd5c
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2024-03-29 22:29:42 +0100

    fix: #7 text fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
-import Logo from "/public/_70fa34e3-d819-4d5b-9aa7-fa543348061c.jpeg"
+import Logo from "@/assets/logo/_70fa34e3-d819-4d5b-9aa7-fa543348061c.jpeg";
 export default function NotFound() {
   return (
-    <main className="bg-black2darkblue-gradient flex min-h-max justify-center mt-navbar">
-      <div className="px-20 justify-center items-center flex flex-col space-y-10 text-white sm:text-3xl text-2xl md:text-4xl">
-        <h1>Błąd 404</h1>
+    <main className="bg-black2darkblue-gradient flex min-h-max justify-center">
+      <div className="px-[5%] justify-center items-center flex flex-col space-y-10 text-close2White">
+        <h1 className="text-center text-2xl md:text-3xl lg:text-4xl">
+          Błąd 404
+        </h1>
         <Image
           src={Logo}
           alt={"Konfero logo"}
@@ -13,10 +15,10 @@ export default function NotFound() {
           className="rounded-3xl"
           priority
         />
-        <h2 className="text-center">
+        <h2 className="text-center text-1xl sm:text-2xl">
           Poszukiwana strona nie istnieje
         </h2>
       </div>
     </main>
-  )
+  );
 }
