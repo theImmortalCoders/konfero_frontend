0e4fb398de3e767b0e1bb39551fb8c56d45cc7b0
Bombkerman <96024122+Bombkerman@users.noreply.github.com>
2024-03-29 21:22:18 +0100

    fix: #7 bigger font
    bigger font and code cleanup

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 import Logo from "/public/_70fa34e3-d819-4d5b-9aa7-fa543348061c.jpeg"
 export default function NotFound() {
   return (
-    <div className="bg-black2darkblue-gradient flex flex-col h-screen items-center justify-center space-y-10">
-      <h1 className="text-1xl sm:text-3xl md:text-4xl text-bold text-white" >Błąd 404</h1>
-      <div className="flex mx-auto">
+    <main className="bg-black2darkblue-gradient flex min-h-max justify-center mt-navbar">
+      <div className="px-20 justify-center items-center flex flex-col space-y-10 text-white sm:text-3xl text-2xl md:text-4xl">
+        <h1>Błąd 404</h1>
         <Image
           src={Logo}
           alt={"Konfero logo"}
@@ -13,12 +13,10 @@ export default function NotFound() {
           className="rounded-3xl"
           priority
         />
-      </div>
-      <div className="mx-auto text-center">
-        <h2 className="text-1xl sm:text-3xl md:text-4xl  text-bold text-white">
+        <h2 className="text-center">
           Poszukiwana strona nie istnieje
         </h2>
       </div>
-    </div>
+    </main>
   )
 }
