8fe657a9c6a041425666c42bc7bc364328886fab
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2024-03-29 22:24:51 +0100

    fix: #18 text fix

--- src/components/common/Error500/page.tsx
+++ src/components/common/Error500/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import Logo from "@/assets/logo/_70fa34e3-d819-4d5b-9aa7-fa543348061c.jpeg"
+import Logo from "@/assets/logo/_70fa34e3-d819-4d5b-9aa7-fa543348061c.jpeg";
 export default function Error500() {
   return (
     <main className="bg-black2darkblue-gradient flex min-h-max justify-center">
-      <div className="px-20 justify-center items-center flex flex-col space-y-10 text-white sm:text-3xl text-2xl md:text-4xl">
-        <h1 className="text-center">
-          Ups, coś poszło nie tak.
+      <div className="px-[5%] justify-center items-center flex flex-col space-y-10 text-close2White ">
+        <h1 className="text-center text-2xl md:text-3xl lg:text-4xl">
+          Występują problemy techniczne
         </h1>
         <Image
           src={Logo}
@@ -15,9 +15,8 @@ export default function Error500() {
           className="rounded-3xl"
           priority
         />
-        <h3 className="text-center">Błąd wewnętrzny serwera</h3>
-        <h2>500</h2>
+        <h3 className="text-center text-1xl sm:text-2xl">Zapraszamy później</h3>
       </div>
     </main>
-  )
+  );
 }
