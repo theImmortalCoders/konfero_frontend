c290d37a8be7e48dc7fd15e577f2773c6ab9a120
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2024-04-03 13:55:08 +0200

    refactor: #25 Navbar

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -7,59 +7,57 @@ import {
   IoMenuOutline,
   IoCloseOutline,
 } from "react-icons/io5";
+import Link from "next/link";
 
+function Navbox() {
+  return (
+    <>
+      <Link href="/aboutus">
+        <button>O nas</button>
+      </Link>
+      <Link href="/conference">
+        <button>Konferencje</button>
+      </Link>
+      <Link href="/myconference">
+        <button>Moje konferencje</button>
+      </Link>
+    </>
+  );
+}
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
   return (
     <nav className="max-w-screen h-navbar bg-close2White flex items-center justify-between px-4 md:px-8 shadow-navbarShadow sticky z-20">
-      <a href="/">
+      <Link href="/">
         <Image src={Logo} alt="Logo" width={70} height={70} />
-      </a>
+      </Link>
 
-      <div className="text-darkblue font-sans text-md md:text-lg lg:text-xl md:flex md:flex-row md:gap-10 hidden">
-        <a href="/aboutus">
-          <button>O nas</button>
-        </a>
-        <a href="/conference">
-          <button>Konferencje</button>
-        </a>
-        <a href="/myconference">
-          <button>Moje konferencje</button>
-        </a>
+      <div className="text-darkblue font-sans text-md lg:text-lg md:flex md:flex-row md:gap-10 hidden">
+        <Navbox />
       </div>
 
       <div className="flex flex-row gap-4 md:gap-8 items-center">
-        <a href="/login">
+        <Link href="/login">
           <IoPersonCircleOutline className="w-8 h-8 text-darkblue" />
-        </a>
-        <div className="md:hidden">
-          <button
-            onClick={toggleMenu}
-            className="text-darkblue mr-2 flex items-center"
-          >
-            {isMenuOpen ? (
-              <IoCloseOutline className="w-8 h-8" />
-            ) : (
-              <IoMenuOutline className="w-8 h-8" />
-            )}
-          </button>
-        </div>
+        </Link>
+        <button
+          onClick={toggleMenu}
+          className="text-darkblue mr-2 flex items-center md:hidden"
+        >
+          {isMenuOpen ? (
+            <IoCloseOutline className="w-8 h-8" />
+          ) : (
+            <IoMenuOutline className="w-8 h-8" />
+          )}
+        </button>
       </div>
 
       {isMenuOpen && (
         <div className="md:hidden absolute top-navbar left-0 w-full bg-close2White shadow-md py-4">
           <div className="flex flex-col items-center gap-4">
-            <a href="/aboutus">
-              <button>O nas</button>
-            </a>
-            <a href="/conference">
-              <button>Konferencje</button>
-            </a>
-            <a href="/myconference">
-              <button>Moje konferencje</button>
-            </a>
+            <Navbox />
           </div>
         </div>
       )}
