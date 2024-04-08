835edc8c337f0d299b1d4577942439477f4d8799
Bartłomiej Krówka <157894166+bkrowka@users.noreply.github.com>
2024-04-08 11:42:39 +0200

    feat: #34 Basic BecomeOrganiser Page Layout + RWD

---
+++ src/app/(role_user)/becomeorganiser/page.tsx
@@ -0,0 +1,33 @@
+import Page from "@/components/common/Page/Page";
+import { Box } from "@/components/common/Box/Box";
+import Image from "next/image";
+import Conference from "@/assets/becomeorganiser/conference.jpg";
+
+function Header ({ children }: { children: React.ReactNode }) {
+    return (
+        <h1 className="text-xl md:text-2xl font-bold mb-4 text-center">{ children }</h1>
+    )
+  }
+
+function Description ({ children }: { children: React.ReactNode }) {
+    return (
+        <p className="text-xs md:text-lg xl:text-base text-center">{ children }</p>
+    )
+  }
+
+export default function BecomeOrganiser () {
+    return (
+        <Page>
+            <Box className="flex flex-col md:flex-row items-center w-[90%] lg:w-[75%] xl:w-[60%] text-darkblue space-x-0 md:space-x-8">
+                <Image src={Conference} alt="Conference" className="w-5/6 md:w-2/5 mt-5 lg:mt-0 rounded-3xl hidden xl:block" />
+                <div className="flex flex-col items-center justify-center">
+                    <Header>Zostań organizatorem konferencji</Header>
+                    <Description>Chcesz zarządzać konferencjami i ich zawartością?</Description>
+                    <Description>Rola organizatora umożliwia dodawanie prelekcji, zapraszanie prelegentów i dystrybucję materiałów dla uczestników.</Description>
+                    <p className="text-sm md:text-lg xl:text-base font-bold my-4 text-center">Wypełnij formularz i ciesz się możliwościami organizatora </p>
+                    <button className="text-nowrap w-auto bg-blue text-close2White text-lg font-medium py-2 px-6 rounded-3xl ">Poproś o dostęp</button>
+                </div>
+            </Box>
+        </Page>
+    )
+}
