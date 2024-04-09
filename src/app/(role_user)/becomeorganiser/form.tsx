49f8a735c6744131069ce2a9e807d11c98aad34c
Bartłomiej Krówka <157894166+bkrowka@users.noreply.github.com>
2024-04-09 15:09:09 +0200

    fix: #34 Floating labels fix

--- src/app/(role_user)/becomeorganiser/form.tsx
+++ src/app/(role_user)/becomeorganiser/form.tsx
@@ -14,7 +14,7 @@ function OrganiserFormInput ({
     type: string;
     id: string;
     name: string;
-    placeholder?: string,
+    placeholder: string,
     value?: string,
     onChange?: React.ChangeEventHandler<HTMLInputElement>; 
   }) {
@@ -41,6 +41,7 @@ function CompanyNameInput () {
                 type="text"
                 id="name"
                 name="companyName"
+                placeholder=" "
                 value={CompanyName}
                 onChange={(e) => {
                     const value = e.target.value;
@@ -52,7 +53,9 @@ function CompanyNameInput () {
                     }
                 }}
             />
-            <label htmlFor="name" className="absolute left-0 top-1 cursor-text peer-focus:text-xs peer-focus:-top-4 text-blue peer-focus:text-darkblue font-sans peer-focus:font-bold transition-all">Nazwa firmy</label>
+            <label htmlFor="name" className="absolute left-0 -top-4 text-xs text-darkblue font-bold cursor-text peer-placeholder-shown:top-1 peer-placeholder-shown:text-base  peer-placeholder-shown:font-normal peer-placeholder-shown:text-blue peer-focus:text-xs peer-focus:-top-4 peer-focus:text-darkblue font-sans peer-focus:font-bold transition-all">
+                Nazwa firmy
+            </label>
         </div>    
     )
 }
@@ -65,6 +68,7 @@ function AddressInput () {
                 type="text"
                 id="address"
                 name="address"
+                placeholder=" "
                 value={address}
                 onChange={(e) => {
                     const value = e.target.value;
@@ -76,7 +80,9 @@ function AddressInput () {
                     }
                 }}
             />
-            <label htmlFor="address" className="absolute left-0 top-1 cursor-text peer-focus:text-xs peer-focus:-top-4 text-blue peer-focus:text-darkblue font-sans peer-focus:font-bold transition-all">Adres</label>
+            <label htmlFor="address" className="absolute left-0 -top-4 text-xs text-darkblue font-bold cursor-text peer-placeholder-shown:top-1 peer-placeholder-shown:text-base  peer-placeholder-shown:font-normal peer-placeholder-shown:text-blue peer-focus:text-xs peer-focus:-top-4 peer-focus:text-darkblue font-sans peer-focus:font-bold transition-all">
+                Adres
+            </label>
         </div>
     )
 }
@@ -89,6 +95,7 @@ function CityInput () {
                 type="text"
                 id="city"
                 name="city"
+                placeholder=" "
                 value={cityName}
                 onChange={(e) => {
                     const value = e.target.value;
@@ -100,7 +107,9 @@ function CityInput () {
                     }
                 }}
             />
-            <label htmlFor="city" className="absolute left-0 top-1 cursor-text peer-focus:text-xs peer-focus:-top-4 text-blue peer-focus:text-darkblue font-sans peer-focus:font-bold transition-all">Miasto</label>
+            <label htmlFor="city" className="absolute left-0 -top-4 text-xs text-darkblue font-bold cursor-text peer-placeholder-shown:top-1 peer-placeholder-shown:text-base  peer-placeholder-shown:font-normal peer-placeholder-shown:text-blue peer-focus:text-xs peer-focus:-top-4 peer-focus:text-darkblue font-sans peer-focus:font-bold transition-all">
+                Miasto
+            </label>
         </div>
     )
 }
@@ -113,6 +122,7 @@ function PhoneNumberInput () {
                 type="text"
                 id="phone"
                 name="phoneNumber"
+                placeholder=" "
                 value={phoneNumber}
                 onChange={(e) => {
                     const value = e.target.value;
@@ -124,7 +134,9 @@ function PhoneNumberInput () {
                     }
                 }}
             />
-            <label htmlFor="phone" className="absolute left-0 top-1 cursor-text peer-focus:text-xs peer-focus:-top-4 text-blue peer-focus:text-darkblue font-sans peer-focus:font-bold transition-all">Numer telefonu</label>
+            <label htmlFor="phone" className="absolute left-0 -top-4 text-xs text-darkblue font-bold cursor-text peer-placeholder-shown:top-1 peer-placeholder-shown:text-base  peer-placeholder-shown:font-normal peer-placeholder-shown:text-blue peer-focus:text-xs peer-focus:-top-4 peer-focus:text-darkblue font-sans peer-focus:font-bold transition-all">
+                Numer telefonu
+            </label>
         </div>
     )
 }
@@ -147,7 +159,7 @@ export default function BecomeOrganiserForm ({
             <Box className="flex flex-col justify-center items-center w-5/6 xs:w-auto text-darkblue space-y-6 z-20">
                 <Image src={Logo} alt="Logo" className="w-36 md:w-48"/>
                 <p className="text-center font-bold">Podaj dane, aby uzyskać status organizatora</p>
-                    <div className="flex flex-col w-full h-fit space-y-6">
+                    <div className="flex flex-col w-full h-fit space-y-8">
                         <CompanyNameInput/>
                         <AddressInput/>
                         <CityInput/>
