e34c4dff1d22bb2fcd3faef3b2ae11219a557de5
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2024-04-15 01:30:19 +0200

    feat: #28 Participants map

--- src/app/(role_organizer)/myconference/[conferenceId]/page.tsx
+++ src/app/(role_organizer)/myconference/[conferenceId]/page.tsx
@@ -62,41 +62,28 @@ export default function MyConferencePage({
           <Box className="text-darkblue w-[90%] lg:w-[60%] mt-5 mb-5">
             <TitleHeader title={"Uczestnicy"} />
             {!conferenceIdData.participantsFull ? (
-              <h1 className="w-full flex justify-center text-sm sm:text-md md:text-lg lg:text-md xl:text-lg">
-                Pozostało x / {conferenceIdData.participantsLimit}
-              </h1>
+              <>
+                <h1 className="w-full flex justify-center text-sm sm:text-md md:text-lg lg:text-md xl:text-lg">
+                  Pozostało{" "}
+                  {conferenceIdData.participantsLimit -
+                    conferenceIdData.participants.length}{" "}
+                  / {conferenceIdData.participantsLimit}
+                </h1>
+                <div className="w-full grid-cols-4 grid gap-8 pt-4">
+                  {conferenceIdData.participants.map((participants, index) => (
+                    <People
+                      key={index}
+                      username={participants.username}
+                      photo={participants.photo}
+                    />
+                  ))}
+                </div>
+              </>
             ) : (
               <h1 className="w-full flex justify-center text-sm sm:text-md md:text-lg lg:text-md xl:text-lg">
                 Niestety brak wolnych miejsc
               </h1>
             )}
-
-            <div className="w-full grid-cols-4 grid gap-8 pt-4">
-              <People
-                username={conferenceIdData.organizer.username}
-                photo={conferenceIdData.organizer.photo}
-              />
-              <People
-                username={conferenceIdData.organizer.username}
-                photo={conferenceIdData.organizer.photo}
-              />
-              <People
-                username={conferenceIdData.organizer.username}
-                photo={conferenceIdData.organizer.photo}
-              />
-              <People
-                username={conferenceIdData.organizer.username}
-                photo={conferenceIdData.organizer.photo}
-              />
-              <People
-                username={conferenceIdData.organizer.username}
-                photo={conferenceIdData.organizer.photo}
-              />
-              <People
-                username={conferenceIdData.organizer.username}
-                photo={conferenceIdData.organizer.photo}
-              />
-            </div>
           </Box>
           <Box className="text-darkblue w-[90%] lg:w-[60%] mt-5 mb-20">
             <TitleHeader title={"Zdjęcia"} />
