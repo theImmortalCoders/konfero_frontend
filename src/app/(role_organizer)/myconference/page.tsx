62e814d94f8ae180dd148a9ed55cc3de1174fb5f
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2024-04-16 11:47:42 +0200

    fix: #30 padding fix in ListItem

--- src/app/(role_organizer)/myconference/page.tsx
+++ src/app/(role_organizer)/myconference/page.tsx
@@ -1,44 +1,45 @@
-'use client'
+"use client";
 
 import Page from "@/components/common/Page/Page";
 import Link from "next/link";
 import { FaPlus } from "react-icons/fa6";
-import ListItem from "@/components/MyconferenceList/ListItem"
-import {getAllConferences, GetAllConferencesData} from "@/hooks/conference"
-import {useRouter} from "next/navigation";
-import {useQuery} from "react-query";
-import {Box} from "@/components/common/Box/Box";
+import ListItem from "@/components/MyconferenceList/ListItem";
+import { getAllConferences, GetAllConferencesData } from "@/hooks/conference";
+import { useRouter } from "next/navigation";
+import { useQuery } from "react-query";
+import { Box } from "@/components/common/Box/Box";
 
 export default function MyConferenceListPage() {
-
   const router = useRouter();
 
-  const {
-    data,
-    status,
-  } = useQuery({
-    queryKey: 'konferencje',
+  const { data, status } = useQuery({
+    queryKey: "konferencje",
     queryFn: getAllConferences,
     staleTime: Infinity,
     refetchOnMount: "always",
-  })
+  });
 
-  return(
+  return (
     <Page>
       <div className="flex flex-col items-center my-8 space-y-7 w-2/3">
         <Box className="flex w-full justify-between text-black font-black">
-          Znalezione konferencje: {(data as GetAllConferencesData)?.numberOfElements}
-          <Link href={'/'} className="flex justify-center items-center gap-x-2 px-2 py-2 sm:py-0 w-min md:text-nowrap border-2 border-black rounded-xl">
+          Znalezione konferencje:{" "}
+          {(data as GetAllConferencesData)?.numberOfElements}
+          <Link
+            href={"/"}
+            className="flex justify-center items-center gap-x-2 px-2 py-2 sm:py-0 w-min md:text-nowrap border-2 border-black rounded-xl"
+          >
             <p className="sm:inline-block hidden">Dodaj Konferencje</p>
-            <FaPlus/>
+            <FaPlus />
           </Link>
         </Box>
-        {
-          status === "success" ?
+        {status === "success" ? (
           (data as GetAllConferencesData)?.content?.map((conf) => {
-            return (<ListItem key={`${(conf).id}`} conference={conf}/>);
-          }) : <></>
-        }
+            return <ListItem key={`${conf.id}`} conference={conf} />;
+          })
+        ) : (
+          <></>
+        )}
       </div>
     </Page>
   );
--- src/components/MyconferenceList/ListItem.tsx
+++ src/components/MyconferenceList/ListItem.tsx
@@ -1,48 +1,43 @@
-'use client'
-import {Box} from "@/components/common/Box/Box";
+"use client";
 import APIImageComponent from "@/hooks/imageAPI";
 import Link from "next/link";
 import ListItemOptions from "@/components/MyconferenceList/ListItemOptions";
-import {Content, deleteConference} from "@/hooks/conference"
-import {useCallback, useState} from "react";
-export default function ListItem({conference} : {conference: Content}) {
+import { Content, deleteConference } from "@/hooks/conference";
+import { useCallback, useState } from "react";
 
-    const [deleted, setDeleted] = useState<boolean>(false);
+export default function MaterialBox({ conference }: { conference: Content }) {
+  const [deleted, setDeleted] = useState<boolean>(false);
 
-    const handleDelete =
-      useCallback(() => {
-        deleteConference(conference?.id);
-        setDeleted(true);
-        }, [])
+  const handleDelete = useCallback(() => {
+    deleteConference(conference?.id);
+    setDeleted(true);
+  }, []);
 
-    function formatDate(dateToFormat: string) {
-      return new Date(dateToFormat).toLocaleString();
-    }
+  function formatDate(dateToFormat: string) {
+    return new Date(dateToFormat).toLocaleString();
+  }
 
-    return deleted?(<></>):(
-    <Box className="flex w-full text-black">
-      <Link href={`/myconference/${conference?.id}`} className="w-full px-3.5 hover:bg-gray-200 rounded-3xl duration-200">
-        <div className="flex items-center w-full space-x-3.5 md:h-28">
-          <div className="md:flex hidden rounded-xl overflow-hidden h-min max-h-full max-w-[20%]">
-            <APIImageComponent imageId={conference.logo.id} type={'IMAGE'}/>
+  return deleted ? (
+    <></>
+  ) : (
+    <div className="flex w-full text-black bg-close2White items-start shadow-whiteShadow h-auto z-0 rounded-3xl">
+      <Link
+        href={`/myconference/${conference?.id}`}
+        className="w-full hover:bg-gray-200 rounded-l-3xl duration-200"
+      >
+        <div className="flex items-center w-full space-l-3.5 md:h-28">
+          <div className="md:flex hidden rounded-l-3xl overflow-hidden h-min max-h-full max-w-[20%] pr-3">
+            <APIImageComponent imageId={conference.logo.id} type={"IMAGE"} />
           </div>
           <div className="flex flex-col">
-            <p className="font-black text-xl">
-              {conference?.name}
-            </p>
-            <p>
-              Rozpoczęcie: {formatDate(conference?.startDateTime)}
-            </p>
-            <p>
-              Zakończenie: {formatDate(conference?.endDateTime)}
-            </p>
-            <p className="font-bold">
-              {conference?.location.name}
-            </p>
+            <p className="font-black text-xl">{conference?.name}</p>
+            <p>Rozpoczęcie: {formatDate(conference?.startDateTime)}</p>
+            <p>Zakończenie: {formatDate(conference?.endDateTime)}</p>
+            <p className="font-bold">{conference?.location.name}</p>
           </div>
         </div>
       </Link>
-      <ListItemOptions confId={conference?.id} handleDelete={handleDelete}/>
-    </Box>
+      <ListItemOptions confId={conference?.id} handleDelete={handleDelete} />
+    </div>
   );
 }
