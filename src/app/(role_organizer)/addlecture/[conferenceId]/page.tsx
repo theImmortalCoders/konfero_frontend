2e5c5c5bdc7fe4bad28343e32b8da82135515539
Paweł Buczek <pbuczek7@wp.pl>
2024-04-17 01:56:56 +0200

    feat: #42 handling invalid url conferenceId with useQuery

--- src/app/(role_organizer)/addlecture/[conferenceId]/page.tsx
+++ src/app/(role_organizer)/addlecture/[conferenceId]/page.tsx
@@ -1,9 +1,36 @@
+"use client";
 import Page from "@/components/common/Page/Page";
+import NotFoundConferenceForAddLecture from "./not-found";
+import { useQuery } from "react-query";
+import { getConferenceDetailsWithRoleFiltering } from "@/hooks/conference";
 
-export default function addlecture() {
+export default function addlecture({ params }: { params: { conferenceId: number } }) {
+
+  const getConferenceInfo = async () => {
+    return await getConferenceDetailsWithRoleFiltering(params.conferenceId);
+  };
+
+  const {
+    data: conferenceData,
+    isLoading: conferenceLoading,
+    isError: conferenceError,
+  } = useQuery("conference", getConferenceInfo);
+
+  if (conferenceError) {
+    return <NotFoundConferenceForAddLecture />;
+  }
+
   return (
     <Page>
-      addlecture
+      {!conferenceLoading && conferenceData && typeof conferenceData !== "string" ? (
+        <>
+          addlecture {params.conferenceId}
+        </>
+      ) : (
+        <p className="w-full mt-20 text-xl h-max flex justify-center items-center text-close2White">
+          Trwa ładowanie danych...
+        </p>
+      )}
     </Page>
   );
 }
