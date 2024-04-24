e629b1ce78d935e9a1c642b615a49645fe17e4a2
Bombkerman <96024122+Bombkerman@users.noreply.github.com>
2024-04-24 18:05:04 +0200

    feat: #52 link to updating a lecture

--- src/components/lecture/MyLecturePageImageBox.tsx
+++ src/components/lecture/MyLecturePageImageBox.tsx
@@ -1,14 +1,26 @@
 import { GetLectureDetailsData } from "@/hooks/lecture";
 import { FaRegCalendarCheck, FaRegClock } from "react-icons/fa6";
 import { IoMdPin } from "react-icons/io";
+import { MdEdit } from "react-icons/md";
+import { useRouter } from "next/navigation";
 
 export default function MyLecturePageImageBox({
   lectureIdData,
 }: {
   lectureIdData: GetLectureDetailsData;
 }) {
+
+  const router = useRouter()
+
   return (
     <div className="absolute top-0 left-0 w-full h-fit flex flex-col items-center text-close2White p-4">
+      <p
+        onClick={() => router.push(`/updatelecture/${lectureIdData.id}`)}
+        className="flex absolute right-4 top-4 px-3 py-1 justify-center items-center space-x-2 bg-close2White rounded-3xl font-black text-darkblue cursor-pointer"
+      >
+        <MdEdit className="text-2xl"/>
+        <p className="hidden md:block" >Edytuj wykład</p>
+      </p>
       <h1 className="text-lg sm:text-2xl  md:text-3xl lg:text-2xl xl:text-3xl pt-[5%]">
         {lectureIdData.name}
       </h1>
