e15f71ab005b5aa658073aa71a971052d119ab5c
Bombkerman <96024122+Bombkerman@users.noreply.github.com>
2024-05-02 21:11:34 +0200

    fix: #71 integrated tags without responsive design

--- src/components/myconference/list/ConferenceList.tsx
+++ src/components/myconference/list/ConferenceList.tsx
@@ -6,9 +6,9 @@ import { CiCirclePlus } from "react-icons/ci";
 import { useRouter } from "next/navigation";
 
 export default function ConferenceList({
-  conference,
-  role,
-}: {
+   conference,
+   role,
+ }: {
   conference: Content;
   role: string;
 }) {
@@ -21,18 +21,18 @@ export default function ConferenceList({
     }
   };
   return (
-    <div className="flex w-full text-black bg-close2White items-start shadow-whiteShadow h-full z-0 rounded-3xl">
+    <div className="flex flex-col xs:flex-row items-center xs:items-start w-full text-black bg-close2White hover:bg-gray-200 duration-200 shadow-whiteShadow h-full z-0 rounded-3xl relative pb-4 xs:py-0">
       <ListItemImage
         href={`/myconference/${conference.id}`}
         logo={conference.logo}
         className="rounded-l-3xl"
       >
         <div className="flex flex-col w-4/5">
-          <div className="flex flex-col pl-4 space-y-0 pt-1">
-            <p className="font-black text-lg xs:text-xl">{conference?.name}</p>
-            <p className="text-sm xs:text-base">{formatDate(conference?.startDateTime)}
-            &nbsp;- {formatDate(conference?.endDateTime)}</p>
-            <p className="font-semibold xs:font-bold text-sm xs:text-base">{conference?.location?.name}</p>
+          <div className="flex flex-col xs:pl-4 gap-y-0 xs:gap-y-1 py-2 w-full items-center xs:items-start text-center break-all 2xs:break-normal">
+            <p className="font-black text-sm 2xs:text-xl">{conference?.name}</p>
+            <p className="text-xs 2xs:text-base">{formatDate(conference?.startDateTime)}
+              &nbsp;- {formatDate(conference?.endDateTime)}</p>
+            <p className="font-semibold xs:font-bold text-xs 2xs:text-base">{conference?.location?.name}</p>
           </div>
           <div className="flex flex-row w-full pl-4 items-center justify-start mt-1 mb-2 space-x-1">
             {conference.tags?.slice(0, 8).map((tag) => (
@@ -47,9 +47,12 @@ export default function ConferenceList({
         </div>
       </ListItemImage>
       {(role === "USER" || role === "ALL") && (
-        <div className="w-auto h-full flex justify-center items-center">
+        <div
+          className="w-auto h-full flex justify-center items-center xs:h-min gap-x-2 xs:mr-4 xs:mt-4 px-2 xs:px-0 bg-gray-300 xs:bg-transparent rounded-full cursor-pointer"
+          onClick={handleCirclePlusClick}
+        >
+          <p className="font-semibold text-xs 2xs:text-base hidden 2xs:block xs:hidden ">Dołącz</p>
           <CiCirclePlus
-            onClick={handleCirclePlusClick}
             className="text-4xl text-darkblue"
           />
         </div>
