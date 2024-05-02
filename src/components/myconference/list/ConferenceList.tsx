624019a34af09b0f8760cce26c7909af118607cd
Bombkerman <96024122+Bombkerman@users.noreply.github.com>
2024-05-02 20:14:38 +0200

    fix: #71 showing list elements correct

--- src/components/myconference/list/ConferenceList.tsx
+++ src/components/myconference/list/ConferenceList.tsx
@@ -21,23 +21,26 @@ export default function ConferenceList({
     }
   };
   return (
-    <div className="flex w-full text-black bg-close2White items-start shadow-whiteShadow h-full z-0 rounded-3xl">
+    <div className="flex flex-col xs:flex-row items-center xs:items-start w-full text-black bg-close2White hover:bg-gray-200 duration-200 shadow-whiteShadow h-full z-0 rounded-3xl relative pb-4 xs:py-0">
       <ListItemImage
         href={`/myconference/${conference.id}`}
         logo={conference.logo}
         className="rounded-l-3xl"
       >
-        <div className="flex flex-col pl-4 space-y-0 xs:space-y-1 py-2">
-          <p className="font-black text-lg xs:text-xl">{conference?.name}</p>
-          <p className="text-sm xs:text-base">{formatDate(conference?.startDateTime)}
+        <div className="flex flex-col xs:pl-4 gap-y-0 xs:gap-y-1 py-2 w-full items-center xs:items-start text-center break-all 2xs:break-normal">
+          <p className="font-black text-sm 2xs:text-xl">{conference?.name}</p>
+          <p className="text-xs 2xs:text-base">{formatDate(conference?.startDateTime)}
           &nbsp;- {formatDate(conference?.endDateTime)}</p>
-          <p className="font-semibold xs:font-bold text-sm xs:text-base">{conference?.location?.name}</p>
+          <p className="font-semibold xs:font-bold text-xs 2xs:text-base">{conference?.location?.name}</p>
         </div>
       </ListItemImage>
       {(role === "USER" || role === "ALL") && (
-        <div className="w-auto h-full flex justify-center items-center">
+        <div
+          className="w-auto h-full flex justify-center items-center xs:h-min gap-x-2 xs:mr-4 xs:mt-4 px-2 xs:px-0 bg-gray-300 xs:bg-transparent rounded-full cursor-pointer"
+          onClick={handleCirclePlusClick}
+        >
+          <p className="font-semibold text-xs 2xs:text-base hidden 2xs:block xs:hidden ">Dołącz</p>
           <CiCirclePlus
-            onClick={handleCirclePlusClick}
             className="text-4xl text-darkblue"
           />
         </div>
