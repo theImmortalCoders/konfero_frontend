3a5ad848229484849e0678225303d7bd8d5a0990
Paweł Buczek <pbuczek7@wp.pl>
2024-05-02 20:28:34 +0200

    feat: #69 showing tags under main info

--- src/components/myconference/list/ConferenceList.tsx
+++ src/components/myconference/list/ConferenceList.tsx
@@ -27,19 +27,23 @@ export default function ConferenceList({
         logo={conference.logo}
         className="rounded-l-3xl"
       >
-        <div className="flex flex-col pl-4 space-y-0 xs:space-y-1 py-2 w-2/5">
-          <p className="font-black text-lg xs:text-xl">{conference?.name}</p>
-          <p className="text-sm xs:text-base">{formatDate(conference?.startDateTime)}
-          &nbsp;- {formatDate(conference?.endDateTime)}</p>
-          <p className="font-semibold xs:font-bold text-sm xs:text-base">{conference?.location?.name}</p>
-        </div>
-        <div className="grid grid-cols-3 gap-x-2 gap-y-1 ml-6 items-end justify-center">
-          {conference.tags.slice(0, 8).map((tag) => (
-            <p key={tag.id} className="w-28 overflow-hidden overflow-ellipsis whitespace-nowrap text-center bg-gray-200 border-[1px] border-blue rounded-lg px-1">
-              {tag.tagName}
-            </p>
-          ))}
-          {conference.tags.length > 8 && <p className="w-28 text-center bg-gray-200 border-[1px] border-blue rounded-lg px-1">...</p>}
+        <div className="flex flex-col w-4/5">
+          <div className="flex flex-col pl-4 space-y-0 pt-1">
+            <p className="font-black text-lg xs:text-xl">{conference?.name}</p>
+            <p className="text-sm xs:text-base">{formatDate(conference?.startDateTime)}
+            &nbsp;- {formatDate(conference?.endDateTime)}</p>
+            <p className="font-semibold xs:font-bold text-sm xs:text-base">{conference?.location?.name}</p>
+          </div>
+          <div className="flex flex-row w-full pl-4 items-center justify-start mt-1 mb-2 space-x-1">
+            {conference.tags.slice(0, 8).map((tag) => (
+              <p key={tag.id} className="w-20 h-fit text-xxs overflow-hidden overflow-ellipsis whitespace-nowrap text-center bg-gray-200 border-[1px] border-blue rounded-lg px-1">
+                {tag.tagName}
+              </p>
+            ))}
+            {conference.tags.length > 8 && 
+              <p className="w-20 h-fit text-xxs overflow-hidden overflow-ellipsis whitespace-nowrap text-center bg-gray-200 border-[1px] border-blue rounded-lg px-1">{conference.tags.length - 8} więcej...</p>
+            }
+          </div>
         </div>
       </ListItemImage>
       {(role === "USER" || role === "ALL") && (
