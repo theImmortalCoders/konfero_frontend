10c466775f67cf03afd87a0487bc290cc53555d4
Bartłomiej Krówka <157894166+bkrowka@users.noreply.github.com>
2024-05-08 22:12:35 +0200

    feat: #80 Signing up/out (myconference/id)

--- src/components/conference/SignUpWarning.tsx
+++ src/components/conference/SignUpWarning.tsx
@@ -10,7 +10,7 @@ export default function SignUpWarning({
 } : { 
     setSignUpWarning: Dispatch<SetStateAction<boolean>>;
     tempId: number;
-    setTempId: Dispatch<SetStateAction<number>>;
+    setTempId?: Dispatch<SetStateAction<number>>;
     update: boolean;
     setUpdate: Dispatch<SetStateAction<boolean>>;
 }) {
@@ -21,7 +21,8 @@ export default function SignUpWarning({
             const result = await signUpForConference(tempId);
             if (result === 200) {
               setSignUpWarning(false);
-              setTempId(-1);
+              if (setTempId)
+                setTempId(-1);
               setUpdate(!update);
             } else {
               console.error("Błąd zapisywania na konferencję");
@@ -35,7 +36,7 @@ export default function SignUpWarning({
 
     return (
         <div className="fixed flex items-center justify-center inset-0 z-10">
-            <div onClick={() => {setSignUpWarning(false); setTempId(-1)}} className="absolute inset-0 bg-darkblue opacity-80"></div>
+            <div onClick={() => {setSignUpWarning(false); setTempId && setTempId(-1)}} className="absolute inset-0 bg-darkblue opacity-80"></div>
             <div className="flex flex-col justify-center items-center w-64 sm:w-96 border-2 border-black text-black bg-close2White rounded-xl space-y-2 p-4 z-20">
                 <h1 className="text-xl">Zapis na konferencję</h1>
                 <p className="text-sm text-center font-sans">
