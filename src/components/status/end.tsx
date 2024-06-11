eae6f7dd601bd11069b847f97263ede8d247e3bf
Marcin Bator <marcinbator.ofc@gmail.com>
2024-06-11 15:34:59 +0200

    fix: #dev status fix

--- src/components/status/end.tsx
+++ src/components/status/end.tsx
@@ -4,7 +4,6 @@ import {RiVerifiedBadgeFill} from "react-icons/ri";
 export default function End() {
   return (
     <div className="flex items-center justify-center gap-x-1">
-        <RiVerifiedBadgeFill/>
         <p>Zakończona</p>
     </div>
   );
